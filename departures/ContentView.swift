9904567c1be66e5685ab2062c1db386af15b73c6
Dominik Dvoracek <dominik.dvoracek@productboard.com>
2026-05-31 21:10:36 +0200

    Initial Commit

---
+++ departures/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  departures
+//
+//  Created by Dominik Dvoracek on 31.05.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ departures/departuresApp.swift
@@ -0,0 +1,17 @@
+//
+//  departuresApp.swift
+//  departures
+//
+//  Created by Dominik Dvoracek on 31.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct departuresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
